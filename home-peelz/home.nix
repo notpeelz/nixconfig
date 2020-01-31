a8095513776426e807a3b154d2053ea6c9d94fe7
peelz <LouisTakePILLz@users.noreply.github.com>
2020-01-31 02:42:04 -0500

    Add st terminal

--- home-peelz/home.nix
+++ home-peelz/home.nix
@@ -17,9 +17,25 @@
 
     # General programs
     htop
-    rxvt_unicode kitty
     tmux
     neovim
+
+    # Terminal emulators
+    rxvt_unicode kitty
+    (st.override {
+      conf = builtins.readFile ./st-config.h;
+      patches = builtins.map fetchurl [
+        #{ url = "https://st.suckless.org/patches/xresources/st-xresources-20190105-3be4cf1.diff";
+        #  sha256 = "112zi7jqzj6601gp54nr4b7si99g29lz61c44rgcpgpfddwmpibi";
+        #}
+        { url = "https://st.suckless.org/patches/scrollback/st-scrollback-0.8.2.diff";
+          sha256 = "0rnigxkldl22dwl6b1743dza949w9j4p1akqvdxl74gi5z7fsnlw";
+        }
+        #{ url = "https://st.suckless.org/patches/scrollback/st-scrollback-mouse-0.8.2.diff";
+        #  sha256 = "1fm1b3yxk9ww2cz0dfm67l42a986ykih37pf5rkhfp9byr8ac0v1";
+        #}
+      ];
+    })
 
     # Misc programs
     stow
