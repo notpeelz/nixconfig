1c200f24d999fca7b0e415fc372af789d2eddbdd
peelz <LouisTakePILLz@users.noreply.github.com>
2020-02-01 06:15:05 -0500

    Attempt to fix mounts occasionally failing

--- hosts/peelz-pc/persistence.nix
+++ hosts/peelz-pc/persistence.nix
@@ -7,18 +7,18 @@
   # Persistence of nixos config files
   fileSystems."/etc/nixos" = {
     device = "/persistent/etc/nixos";
-    options = [ "bind" "noauto" "x-systemd.automount" ];
+    options = [ "bind" ];
   };
 
   # Persistence of home folder
   fileSystems."/home/peelz" = {
     device = "/persistent/home/peelz";
-    options = [ "bind" "noauto" "x-systemd.automount" ];
+    options = [ "bind" ];
   };
 
   # Persistence of root home folder
   fileSystems."/root" = {
     device = "/persistent/root";
-    options = [ "bind" "noauto" "x-systemd.automount" ];
+    options = [ "bind" ];
   };
 }
