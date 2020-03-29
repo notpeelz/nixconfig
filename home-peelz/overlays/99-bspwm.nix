d48f5ca6be210687bb9ae6e83de32b565780cba5
peelz <LouisTakePILLz@users.noreply.github.com>
2020-03-29 19:52:51 -0400

    Change github .patch urls to .diff
    GitHub seems to serve non-deterministic patches, which causes the sha256
    to change.

--- home-peelz/overlays/99-bspwm.nix
+++ home-peelz/overlays/99-bspwm.nix
@@ -3,8 +3,9 @@ final: super: {
   bspwm = super.bspwm.overrideAttrs ({ patches ? [ ], ... }: {
     patches = patches ++ map super.fetchpatch [
       # Fixes windows not getting resized properly when ignoring fullscreen events
-      { url = "https://github.com/louistakepillz/bspwm/commit/538d6197532fcf8547548b68dac6b511de57232e.patch";
-        sha256 = "072q2pg31vn52hb3b4q9v0m7cqbf7ibhy8y6rpmp3pcmd1ddmwzj";
+      { url = "https://github.com/louistakepillz/bspwm/commit/538d6197532fcf8547548b68dac6b511de57232e.diff";
+        sha256 = "09qkk0w21wdqg95mrvj7mg09gf2bxz1rh3rc51dzyj2wg4xq0q26";
+      }
       }
     ];
   });
--- home-peelz/overlays/99-zsh-sigwinch.nix
+++ home-peelz/overlays/99-zsh-sigwinch.nix
@@ -6,12 +6,12 @@ final: super: {
   zsh = super.zsh.overrideAttrs ({ patches ? [ ], ... }: {
     patches = patches ++ map super.fetchpatch [
       {
-        url = "https://github.com/louistakepillz/zsh/commit/f016535cb6fd466207d16770d3dcedfafc1799e9.patch";
-        sha256 = "06r6qpmsnwv0my44pim8vx311byf2h35y9xg3gpcchkxrhfngnws";
+        url = "https://github.com/louistakepillz/zsh/commit/f016535cb6fd466207d16770d3dcedfafc1799e9.diff";
+        sha256 = "189fhfcjk1l2zhpxw5r754nxpfhxl2b0ixwhy2xk91fmsbakz0i9";
       }
       {
-        url = "https://github.com/louistakepillz/zsh/commit/f5bf5a014675d3b8ff5c1da9f4de42363f0ba2aa.patch";
-        sha256 = "0cfpnp2y4izzqlsylia2h8y2bgi8yarwjp59kmx6bcvd2vvv5bcx";
+        url = "https://github.com/louistakepillz/zsh/commit/f5bf5a014675d3b8ff5c1da9f4de42363f0ba2aa.diff";
+        sha256 = "0nr2dw3pwqz0x906d24dhqvw9wmvy7c72rgws4rmg327qk1abvfc";
       }
     ];
   });
