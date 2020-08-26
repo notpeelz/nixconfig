ccb4e287fdef7986f14d2132cf08ce2342c64d97
peelz <LouisTakePILLz@users.noreply.github.com>
2020-08-26 17:17:06 -0400

    essential-pkgs: add pkgs

--- common/essential-pkgs.nix
+++ common/essential-pkgs.nix
@@ -5,11 +5,20 @@ pkgs: with pkgs; [
   jq gnugrep gnused gawk
   gnutar xz zip unzip p7zip unrar
   coreutils binutils diffutils findutils utillinux
+  lm_sensors
+  rename
+  dfc
   dnsutils
+  usbutils
+  dmidecode
+  lshw ethtool
+  hdparm testdisk gptfdisk
+  psmisc # pstree, fuser
   e2fsprogs dosfstools
+  dos2unix
   hexdump xxd
   binutils
   file fd tree
   lsof
-  htop pstree killall
+  htop killall
 ]
