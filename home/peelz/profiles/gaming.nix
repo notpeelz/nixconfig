bfc463e29a1d307332e58f654f765a082bcd3271
peelz <LouisTakePILLz@users.noreply.github.com>
2020-08-26 16:36:32 -0400

    profiles/gaming: remove wine LCtrl workaround

--- home/peelz/profiles/gaming.nix
+++ home/peelz/profiles/gaming.nix
@@ -39,15 +39,5 @@ in {
         desktop = 8;
       }
     ];
-
-    # FIXME: this gets reset when home-manager restarts the setxkbmap service
-    #        and needs to be manually reapplied; see https://github.com/rycee/home-manager/issues/543
-    # TODO: combine this into systemd.user.services.setxkbmap.Service
-    xsession.initExtra = ''
-      # Fix Overwatch (Lutris) detecting RCtrl instead of LCtrl
-      # https://bugs.winehq.org/show_bug.cgi?id=45148
-      # Fixed in wine >=5.0_rc1 (2019-12-10)
-      xmodmap -e "keycode 37 = Control_R NoSymbol Control_R" &
-    '';
   };
 }
