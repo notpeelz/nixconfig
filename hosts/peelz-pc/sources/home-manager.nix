a5b05769230ec320a0ab22544ef1fc62d51b086f
peelz <LouisTakePILLz@users.noreply.github.com>
2020-10-12 22:09:00 -0400

    sources: add update.sh

--- hosts/peelz-pc/sources/home-manager.nix
+++ hosts/peelz-pc/sources/home-manager.nix
@@ -1,4 +1,5 @@
-# following release-20.03
+# remote https://github.com/nix-community/home-manager
+# branch release-20.03
 fetchTarball {
   url = "https://github.com/nix-community/home-manager/archive/e6f96b6aa3e99495f9f6f3488ecf78dd316e5bec.tar.gz";
   sha256 = "1xvxqw5cldlhcl7xsbw11n2s3x1h2vmbm1b9b69a641rzj3srg11";
---
+++ update.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+
+nixcfg="$(realpath "$(dirname "$0")")"
+hostname="$1"
+
+if [[ -z "$hostname" ]]; then
+  hostname="$(hostname)"
+fi
+
+host="$nixcfg/hosts/$hostname"
+
+if [[ -z "$host" || ! -d "$host" ]]; then
+  echo "Invalid host: $hostname"
+  exit 1
+fi
+
+echo "Updating sources for $hostname"
+
+for f in "$host/sources/"*.nix; do
+  src="$(basename -s ".nix" "$f")"
+  remote="$(grep -Po '^# remote \K.*' "$f")"
+  branch="$(grep -Po '^# branch \K.*' "$f")"
+  url="$(grep -Po 'url = "\K.*(?=")' "$f")"
+  rev="$(basename -s ".tar.gz" "$url")"
+
+  if [[ -z "$branch" ]]; then
+    echo "Source $src is using a static rev, skipping."
+    continue
+  fi
+
+  if [[ -z "$remote" ]]; then
+    echo "Invalid remote for $src: $remote"
+    continue
+  fi
+
+  echo "Fetching remote HEAD for $src"
+
+  # convert pr number to something useable by git
+  if [[ "${branch:0:1}" == "#" ]]; then
+    branch="pull/${branch:1}/head"
+  fi
+
+  latest_rev="$(git ls-remote "$remote" "$branch" | grep -Po '^.*(?=\t)')"
+
+  if [[ -z "$latest_rev" ]]; then
+    echo "Failed to fetch latest rev for $src"
+    echo
+    continue
+  fi
+
+  # assuming the remote is github.com
+  newurl="$remote/archive/$latest_rev.tar.gz"
+
+  echo "Updating $src"
+  if [[ "$url" == "$newurl" ]]; then
+    echo "Up to date"
+    echo
+    continue
+  fi
+
+  sed_oldurl="$(echo "$url" | sed -e 's/[]\/$*.^[]/\\&/g')"
+  sed_newurl="$(echo "$newurl" | sed -e 's/[]\/$*.^[]/\\&/g')"
+
+  echo "Current rev: $rev"
+  sha256="$(nix-prefetch-url --type sha256 --unpack "$newurl")"
+
+  sed -i "s/url = \"$sed_oldurl\";/url = \"$sed_newurl\";/" "$f"
+  sed -i "s/sha256 = \".*\";/sha256 = \"$sha256\";/" "$f"
+  echo "Updated rev to: $latest_rev"
+  echo "Update hash to: $sha256"
+
+  echo
+done
