79e8594cf64dd9c3491dd18d6e612e7e48f67b79
peelz <LouisTakePILLz@users.noreply.github.com>
2020-12-06 18:31:08 -0500

    overlays: disable outdated (broken) zsh patches

--- overlays/zsh-sigwinch/default.nix
+++ overlays/zsh-sigwinch/default.nix
@@ -5,8 +5,10 @@ with builtins;
 final: super: {
   zsh = super.zsh.overrideAttrs ({ patches ? [ ], ... }: {
     patches = patches ++ [
-      ./f016535cb6fd466207d16770d3dcedfafc1799e9.diff
-      ./f5bf5a014675d3b8ff5c1da9f4de42363f0ba2aa.diff
+      # Breaks with zsh 5.8
+      # TODO: rebase?
+      # ./f016535cb6fd466207d16770d3dcedfafc1799e9.diff
+      # ./f5bf5a014675d3b8ff5c1da9f4de42363f0ba2aa.diff
     ];
   });
 }
