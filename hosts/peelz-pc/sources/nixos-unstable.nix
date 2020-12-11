bde2838bf677847dee9563bcbcd1d106b2cceefd
peelz <LouisTakePILLz@users.noreply.github.com>
2020-12-10 23:28:28 -0500

    peelz-pc: update sources

--- hosts/peelz-pc/sources/nixos-unstable.nix
+++ hosts/peelz-pc/sources/nixos-unstable.nix
@@ -1,6 +1,6 @@
 # remote https://github.com/NixOS/nixpkgs
 # branch nixos-unstable
 fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/296793637b22bdb4d23b479879eba0a71c132a66.tar.gz";
-  sha256 = "0j09yih9693w5vjx64ikfxyja1ha7pisygrwrpg3wfz3sssglg69";
+  url = "https://github.com/NixOS/nixpkgs/archive/e9158eca70ae59e73fae23be5d13d3fa0cfc78b4.tar.gz";
+  sha256 = "0cnmvnvin9ixzl98fmlm3g17l6w95gifqfb3rfxs55c0wj2ddy53";
 }
--- hosts/peelz-pc/sources/nixpkgs.nix
+++ hosts/peelz-pc/sources/nixpkgs.nix
@@ -1,6 +1,6 @@
 # remote https://github.com/NixOS/nixpkgs
 # branch nixos-20.09
 fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/e34208e10033315fddf6909d3ff68e2d3cf48a23.tar.gz";
-  sha256 = "0ngkx5ny7bschmiwc5q9yza8fdwlc3zg47avsywwp8yn96k2cpmg";
+  url = "https://github.com/NixOS/nixpkgs/archive/65c9cc79f1d179713c227bf447fb0dac384cdcda.tar.gz";
+  sha256 = "0whxlm098vas4ngq6hm3xa4mdd2yblxcl5x5ny216zajp08yp1wf";
 }
