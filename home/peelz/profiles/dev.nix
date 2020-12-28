e5246c60df0c7f98c8617d202dcc91dc8788c3fc
peelz <LouisTakePILLz@users.noreply.github.com>
2020-12-28 18:49:55 -0500

    profiles/dev: fix gdb not loading symbols from /nix/store

--- home/peelz/profiles/dev.nix
+++ home/peelz/profiles/dev.nix
@@ -12,7 +12,22 @@ in {
     home.packages = with pkgs; [
       direnv
       vscode
-      gdb
+      (symlinkJoin {
+        name = "gdb-custom";
+        paths = [
+          gdb
+        ];
+        postBuild = ''
+          # allows loading symbols from the nix store
+          while IFS="" read -r l; do
+            [[ ! -f "$l" ]] && continue
+            source "$l/nix-support/setup-hook"
+          done < ${pkgs.makeWrapper}/nix-support/propagated-build-inputs
+          source ${pkgs.makeWrapper}/nix-support/setup-hook
+          wrapProgram $out/bin/gdb \
+            --add-flags '-iex "set auto-load safe-path /nix"'
+        '';
+      })
       ghidra-bin
       mitmproxy
     ];
